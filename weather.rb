5f690fcad59e54a07a81d125163a750d73cbc067
xiaoming <1406629722@qq.com>
2019-12-02 12:00:01 +0800

    update

--- weather.rb
+++ weather.rb
@@ -1,8 +1,8 @@
 class Weather < Formula
   desc "A command line tool to retrieve local weather"
   homepage "https://github.com/xiaomingfuckeasylife/weather"
-  url "https://github.com/xiaomingfuckeasylife/weather/blob/master/weather-1.0.0.tar.gz"
-  sha256 "3e1fc2e2c2eaac3c703fd44b86646696a61b223fa57d86f50cb920f1ae45ebf4"
+  url "https://github.com/xiaomingfuckeasylife/weather/raw/master/weather-1.0.0.tar.gz"
+  sha256 "286467a38c093d22b2d428e975652de4f48592a4fbe5056572030e460d667002"
   version "1.0.0"
 
   depends_on "curl"
