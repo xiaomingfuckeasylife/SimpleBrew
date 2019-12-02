d223a59ee935103f79db086ef9b170088451f619
xiaoming <1406629722@qq.com>
2019-12-02 17:19:34 +0800

    Fix

--- weather.rb
+++ weather.rb
@@ -3,7 +3,7 @@ class Weather < Formula
   homepage "https://github.com/xiaomingfuckeasylife/weather"
   url "https://github.com/xiaomingfuckeasylife/weather/raw/master/weather-2.0.0.tar.gz"
   sha256 "c547735042463fa3a4d00315b8939a369e8ff39ab9c42a970e9cc14c230a03d3"
-  version "1.0.0"
+  version "2.0.0"
 
   depends_on "curl"
 
